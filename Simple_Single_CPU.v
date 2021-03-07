f13135dadb8e5cb2945b01fe58d8ad2399588477
Heyward <sin55123456@gmail.com>
2021-03-07 18:32:52 +0800

    Feature built: Module Single_Cycle_CPU

--- Simple_Single_CPU.v
+++ Simple_Single_CPU.v
@@ -12,6 +12,7 @@ module Simple_Single_CPU(
 //Internal Signles
 wire [31:0] pc_i;
 wire [31:0] pc_o;
+wire [31:0] pc_4;
 wire [31:0] instr;
 wire [31:0] ALUresult;
 wire RegWrite;
@@ -19,8 +20,22 @@ wire [31:0] RSdata_o;
 wire [31:0] RTdata_o;
 wire ALUSrc;
 wire Branch;
+wire PCSrc;
 wire [1:0] ALUOp;
-		
+wire [31:0] Imm_o;
+wire [31:0] SL1_o;
+wire [31:0] Adder2_o;
+wire [31:0] ALU_Src2;
+wire [3:0] ALU_Ctrl_o;
+wire [3:0] ALU_Ctrl_i;
+wire Zero;
+wire Cout;
+wire Overflow;
+
+assign ALU_Ctrl_i[3] = instr[30];
+assign ALU_Ctrl_i[2:0] = instr[14:12];
+and Beq_AND(PCSrc, Branch, Zero);
+
 ProgramCounter PC(
         .clk_i(clk_i),      
 	    .rst_i (rst_i),     
@@ -41,8 +56,8 @@ Reg_File RF(
         .RDaddr_i(instr[11:7]) ,  
         .RDdata_i(ALUresult)  , 
         .RegWrite_i (RegWrite),
-        .RSdata_o(RSdata_o) ,  
-        .RTdata_o(RTdata_o)   
+        .RSdata_o(RSdata_o) ,  //rs1
+        .RTdata_o(RTdata_o)    //rs2  
         );
 		
 Decoder Decoder(
@@ -54,58 +69,59 @@ Decoder Decoder(
 	    );	
 
 Adder Adder1(
-        .src1_i(),     
-	    .src2_i(),     
-	    .sum_o()    
+            .src1_i(pc_o),     
+	    .src2_i(32'h00000004),     
+	    .sum_o(pc_4)    
 	    );
 		
 Imm_Gen ImmGen(
-		.instr_i(),
-		.Imm_Gen_o()
+		.instr_i(instr[31:0]),
+		.Imm_Gen_o(Imm_o)
 		);
 	
 Shift_Left_1 SL1(
-		.data_i(),
-		.data_o()
+		.data_i(Imm_o),
+		.data_o(SL1_o)
 		);
 	
 MUX_2to1 Mux_ALUSrc(
-		.data0_i(),       
-		.data1_i(),
-		.select_i(),
-		.data_o()
+		.data0_i(RTdata_o),       
+		.data1_i(Imm_o),
+		.select_i(ALUSrc),
+		.data_o(ALU_Src2)
 		);
 			
 ALU_Ctrl ALU_Ctrl(
-		.instr(),
-		.ALUOp(),
-		.ALU_Ctrl_o()
+		.instr(ALU_Ctrl_i),
+		.ALUOp(ALUOp),
+		.ALU_Ctrl_o(ALU_Ctrl_o)
 		);
 		
 Adder Adder2(
-        .src1_i(),     
-	    .src2_i(),     
-	    .sum_o()    
+            .src1_i(pc_o),     
+	    .src2_i(SL1_o),     
+	    .sum_o(Adder2_o)    
 	    );
 		
 alu alu(
-		.rst_n(),
-		.src1(),
-		.src2(),
-		.ALU_control(),
-		.zero(),
-		.result(),
-		.cout(),
-		.overflow()
+		.rst_n(rst_i),
+		.src1(RSdata_o),
+		.src2(ALU_Src2),
+		.ALU_control(ALU_Ctrl_o),
+		.zero(Zero),
+		.result(ALUresult),
+		.cout(Cout),
+		.overflow(Overflow)
 		);
 	
 MUX_2to1 Mux_PCSrc(
-		.data0_i(),       
-		.data1_i(),
-		.select_i(),
-		.data_o()
+		.data0_i(pc_4),       
+		.data1_i(Adder2_o),
+		.select_i(PCSrc),
+		.data_o(pc_i)
 		);
-		
+
+
 endmodule
 		  
 
